77ffa3ae88e0eaa9c9e153454450c5a82caf96d3
Norio <umanari145@gmail.com>
2020-02-26 21:10:40 +0900

    多重変数の定義と多重戻り値

--- main.go
+++ main.go
@@ -9,4 +9,18 @@ func main() {
 	el.Warn("--batch start--")
 
 	el.Warn("--batch end--")
+
+	//PHPでいうlist的な取り出し方
+	x2, y2 := basic_fun("hoge", "foo")
+	el.Warn("x2:" + x2)
+	el.Warn("y2:" + y2)
+}
+
+/**
+ * 引数をまとめた関数
+ */
+func basic_fun(x, y string) (string, string) {
+	x2 := x + "aaa"
+	y2 := y + "aaa"
+	return x2, y2
 }
